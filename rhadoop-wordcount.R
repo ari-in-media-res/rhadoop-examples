73c160bf18456cb4bfd13adcaa5302e63b5a5b4f
Vedant Jain <vedjain85@gmail.com>
2014-07-14 09:38:07 -0500

    rmr-local-wordcount.R
    Rscript running mapreduce on local disk data and writing result to HDFS
    
    Inspired by: https://github.com/ekohlwey/rhadoop-examples/blob/master/Solutions/wordcount-simple.R

---
+++ rhadoop-wordcount.R
@@ -0,0 +1,23 @@
+library(rmr)
+rmr.options.set(backend="local")
+
+wordcount_mapper = function(null,line) {
+  split_words = unlist(strsplit(line, split="\\s+", perl=TRUE))
+  lapply(split_words, function(word) keyval(word,1))
+}
+
+wordcount_reducer = function(word,occurrences) {
+  keyval(word, sum(unlist(occurrences)))
+}
+
+results = mapreduce("~/Data/..../", input.format="text",
+                    map=wordcount_mapper, reduce=wordcount_reducer, 
+                    combine=T)
+
+frequencies = from.dfs(results)
+
+counts = unlist(lapply(frequencies, function(kv) kv$val))
+words = unlist(lapply(frequencies, function(kv) kv$key))
+orders = order(counts,decreasing=T)[1:10]
+
+barplot(counts[orders], names.arg=words[orders] )
